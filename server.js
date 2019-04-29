5c97df0f8ed46ed05b549b748cd84193b3affbda
Paul Vintimilla <pvintimilla@ups.edu.ec>
2019-04-29 01:35:51 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine','hbs')
+
+app.get('/', (req,res) => {
+    res.render('home.hbs',{
+        nombre: 'paul'
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
+app.listen(port, () =>{
+    console.log(`Escuchando peticiones en el puerto ${port}`)
+});
